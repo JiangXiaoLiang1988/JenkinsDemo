f8694565d0a34710aed7b6632507586d85ab6ba6
jiangxiaoliang1988 <jiangxiaoliang@163.com>
2019-08-24 11:42:35 +0800

    first commit code

---
+++ JenkinsMVCDemo/JenkinsMVCDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinsMVCDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
